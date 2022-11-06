1cab863e8d48f047334852c18018e8359e76c8ba
kijanaa <irene.yanapouth@gmail.com>
2022-11-06 21:42:55 +0100

    first commit

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import './Slider.css'
+import dataSlider from './dataSlider'
+import BtnSlider from './BtnSlider';
+
+
+const Slider = () => {
+const [slideAnim, setSlideAnim] = useState({
+    index: 1,   
+    inProgress: false
+});
+
+const nextSlide = () => {
+if (slideAnim.index !== dataSlider.length){
+    setSlideAnim({index: slideAnim.index + 1, inProgress: true})
+}
+else {
+    setSlideAnim({index: 1, inProgress: true})
+}
+}
+
+
+
+
+const prevSlide = () => {
+if (slideAnim.index !== 1){
+    setSlideAnim({index: slideAnim.index - 1, inProgress: true}) 
+}
+else{
+    setSlideAnim({index: dataSlider.length, inProgress: true})
+
+}
+}
+
+  return (
+    <div className="container-slider">
+        {dataSlider.map((obj, index) =>{
+            return(
+                <div
+                    key={obj.id}
+                    className ={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
+                    >
+                        <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt="" />
+                </div>
+            )
+        }
+        )
+        }
+
+        <BtnSlider  moveSlide = {nextSlide} direction= {"next"}/>
+        <BtnSlider  moveSlide = {prevSlide} direction= {"prev"}/>
+
+
+    </div>
+  )
+}
+
+export default Slider;
